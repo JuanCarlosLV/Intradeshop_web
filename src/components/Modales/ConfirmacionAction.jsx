efc09060eda8627b7f923a99e850a5b94013495e
Juan Carlos LV <lopesjuancarlos@gmail.com>
2023-05-11 11:37:35 -0600

    Cambios en el modal

--- src/components/Modales/ConfirmacionAction.jsx
+++ src/components/Modales/ConfirmacionAction.jsx
@@ -1,3 +1,5 @@
+import { IoIosWarning } from "react-icons/io";
+
 function ConfirmacionAction({
   mostrarModal,
   titulo,
@@ -10,43 +12,32 @@ function ConfirmacionAction({
       {mostrarModal && (
         <div className="fixed z-50 inset-0 overflow-y-auto">
           <div className="flex items-center justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+            {/* Esto es para que se ponga oscuro el fondo y evite que se quite el modal */}
             <div
-              className="fixed inset-0 transition-opacity"
+              className="fixed inset-0 transition-opacity bg-gray-500 opacity-75"
               aria-hidden="true"
-            >
-              <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
+            >     
             </div>
+            {/* esto es para posicionar el modal en medio de la pantalla */}
             <span
               className="hidden sm:inline-block sm:align-middle sm:h-screen"
               aria-hidden="true"
             >
-              &#8203;
             </span>
-            <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+
+            <div className="inline-block align-bottom rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
               <div className="bg-[#D1AC00] px-4 pt-5 pb-4 sm:p-20 sm:pb-10">
-                <div className="sm:flex sm:items-start">
-                  <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-5 sm:h-10 sm:w-10">
-                    <svg
-                      className="h-6 w-6 text-black-600"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                      aria-hidden="true"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth={2}
-                        d="M12 6l-6 12h12L12 6z"
-                      />
-                    </svg>
+                <div className=" flex flex-row justify-between ">
+                  <div className="text-[70px] mr-[30px] -ml-10">
+                    <IoIosWarning color="white" />
                   </div>
                   <div>
                     <h3 className="text-3xl leading-6 font-bold font-ralewayFont text-gray-900">
                       {titulo}
                     </h3>
-                    <div className="mt-5 text-xl font-medium font-ralewayFont">{cuerpo}</div>
+                    <div className="mt-5 text-2xl font-medium font-ralewayFont text-left">
+                      {cuerpo}
+                    </div>
                   </div>
                 </div>
               </div>
