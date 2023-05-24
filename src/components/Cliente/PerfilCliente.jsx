7b323ba840838791a2a016b3caf17551e98171f5
Juan Carlos LV <lopesjuancarlos@gmail.com>
2023-05-23 23:41:38 -0600

    cambios

--- src/components/Cliente/PerfilCliente.jsx
+++ src/components/Cliente/PerfilCliente.jsx
@@ -11,33 +11,23 @@ function PerfilCliente() {
       <main>
         <section className="flex flex-row  mt-5 ">
           <NavLink to="/">
-            <AiFillHome color="004643" className="text-[40px] ml-10 mt-2" />
+            <AiFillHome color="004643" className="text-[40px] ml-10 mt-10" />
           </NavLink>
 
-          <div className="bg-[#004643] rounded-[5px] flex flex-row items-center justify-between h-[70px] w-[1330px] ml-[35px]">
+          <div className="bg-[#004643] rounded-[5px] flex flex-row items-center justify-between h-[120px] w-[1500px] ml-[35px]">
             <h1 className="ml-10 font-ralewayFont font-semibold text-2xl text-white">
               MI CUENTA
             </h1>
-            <NavLink className="font-ralewayFont  text-2xl text-[#D1AC00] mr-10">
+            <NavLink className="font-ralewayFont  text-2xl text-[#D1AC00] mr-10"
+            to="editar-perfil">
               Editar
             </NavLink>
           </div>
         </section>
+
         <section className="flex flex-col">
-          <div class="grid divide-y divide-neutral-200 max-w-xl mx-auto mt-8">
-            <div class="py-5">
-              <details class="group">
-                <summary class="flex justify-between items-center font-medium cursor-pointer list-none bg-black text-white">
-                  <span> Datos de la cuenta</span>
-                  <span class="transition group-open:rotate-180">
-                    <IoIosArrowDown />
-                  </span>
-                </summary>
-                <div>
-                  <h1></h1>
-                </div>
-              </details>
-            </div>
+          <div class="grid divide-y divide-[#D1AC00] w-[1500px] ml-[110px] mt-8 ">
+            
             <div class="py-5">
               <details class="group">
                 <summary class="flex justify-between items-center font-medium cursor-pointer list-none">
