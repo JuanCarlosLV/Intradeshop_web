3c6162b4c58e259c469064343680b21ae328e10f
Keila <keymakenzie@gmail.com>
2023-05-30 16:11:31 -0500

    Presentacion

--- src/supabase/connection.js
+++ src/supabase/connection.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = "https://btqlaqloldedvkvyhnzl.supabase.co";
+const supabaseAnonKey ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ0cWxhcWxvbGRlZHZrdnlobnpsIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODAxNDc2MzQsImV4cCI6MTk5NTcyMzYzNH0.zMji4tY1iw8y-Cty-1IANLQBlGxQFLhPwijPKL6skZA";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
