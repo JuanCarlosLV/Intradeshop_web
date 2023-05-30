fef1d5fd3f4c53bac592fcc08be10003ead9d8a1
Keila <keymakenzie@gmail.com>
2023-05-30 15:41:44 -0500

    Ultima actualizacion

--- src/components/Products/GetProducto.jsx
+++ src/components/Products/GetProducto.jsx
@@ -32,78 +32,86 @@ function GetProducto() {
 
   return (
     <>
-      <div className="flex flex-row items-center absolute top-40 right-20 m-1 mt-10">
-        <VscSearch
-          className="text-3xl  ml-[135px] mr-auto block absolute "
-          color="#004643"
-        />
-        <input
-          id="barra de busqueda"
-          placeholder="Buscar por nombre del producto"
-          onChange={handleConsultar}
-          className="px-14  text-[#004643] text-xl w-[900px] h-[50px] rounded-2xl border-b-[6px]  border-[#004643] focus:border-[#004643] focus:outline-none focus:ring-1 focus:ring-[#004643] font-ralewayFont ml-[120px] hover:bg-green-50"
-        ></input>
-      </div>
+          <main className="flex flex-col">
+        <div className="flex flex-row items-center absolute ml-[200px] mt-10">
+          <VscSearch
+            className="text-3xl  ml-[135px] mr-auto block absolute "
+            color="#004643"
+          />
+          <input
+            id="barra de busqueda"
+            placeholder="Buscar por nombre del producto"
+            onChange={handleConsultar}
+            className="px-14  text-[#004643] text-xl w-[900px] h-[50px] rounded-2xl border-b-[6px]  border-[#004643] focus:border-[#004643] focus:outline-none focus:ring-1 focus:ring-[#004643] font-ralewayFont ml-[120px] hover:bg-green-50"
+          ></input>
+        </div>
 
-      <div className="relative overflow-x-auto shadow-md sm:rounded-lg top-40 mx-auto max-w-4xl mt-5 mb-8  float-right mr-60">
-        <table className="w-full text-sm text-left text-gray-700 dark:text-gray-400">
-          <thead className="w-full text-xs text-gray-700 uppercase bg-[#D1AC00] dark:bg-gray-700 dark:text-gray-500 font-medium font-ralewayFont">
-            <tr>
-              <th scope="col" className="px-4 py-3 text-base text-center">
-                id
-              </th>
-              <th scope="col" className="px-4 py-3 text-base text-center">
-                nombre
-              </th>
-              <th scope="col" className="px-4 py-3 text-base text-center">
-                cantidad
-              </th>
-              <th scope="col" className="px-4 py-3 text-base text-center">
-                Acciones
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {productos.map((producto) => (
-              <tr key={producto.idProducto} className="bg-gray-100 border-b dark:bg-white-800 dark:border-gray-700">
-                <td className="px-4 py-3 text-base text-center font-medium font-ralewayFont">
-                  {producto.idProducto}
-                </td>
-                <td className="px-4 py-3 text-base text-center font-medium font-ralewayFont">
-                  {producto.nomProducto}
-                </td>
-                <td className="px-4 py-3 text-base text-center font-medium font-ralewayFont">
-                  {producto.cantidadTotal}
-                </td>
-                <td className="px-4 py-3 text-base text-center font-medium font-ralewayFont">
-
-                  <button
-                    className="font-medium text-blue-600 dark:text-blue-500 hover:underline px-2 py-1 text-base"
-                    onClick={() => handleNavigateEditar(producto.idProducto)}
-                  >
-                    Editar
-                  </button>
-                  <button
-                    className="font-medium text-red-600 dark:text-blue-500 hover:underline px-2 py-2 text-base font-ralewayFont"
-                    onClick={() =>
-                      handleNavigateEliminar(producto.idProducto)
-                    }
-                  >
-                    Eliminar
-                  </button>
-                  <button
-                    className="font-medium text-green-600 dark:text-blue-500 hover:underline px-2 py-2 text-base font-ralewayFont"
-                    onClick={() => handleNavigateDetalle(producto.idProducto)}
-                  >
-                    Detalle
-                  </button>
-
-                </td>
+        <div className="relative overflow-x-auto sm:rounded-lg  mt-[140px]  mb-8 ml-[350px] mr-[200px] ">
+          <table className="w-[1350px] text-sm text-left text-gray-700 dark:text-gray-400">
+            <thead className="w-[1350px] text-xs text-gray-700 uppercase bg-[#D1AC00]  font-medium font-ralewayFont">
+              <tr>
+                <th scope="col" className="px-4 py-3 text-base text-center">
+                  id
+                </th>
+                <th scope="col" className="px-4 py-3 text-base text-center">
+                  nombre
+                </th>
+                <th scope="col" className="px-4 py-3 text-base text-center">
+                  cantidad
+                </th>
+                <th scope="col" className="text-base text-center ">
+                  Acciones
+                </th>
+                <th scope="col" className="text-base text-center ">
+                </th>
+                
+                <th scope="col" className="text-base text-center ">
+                </th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+            </thead>
+            <tbody>
+              {productos.map((producto) => (
+                <tr
+                  key={producto.idProducto}
+                  className="bg-gray-100 border-b dark:bg-white-800 dark:border-gray-700"
+                >
+                  <td className="px-4 py-3 text-base text-center font-medium font-ralewayFont">
+                    {producto.idProducto}
+                  </td>
+                  <td className="px-4 py-3 text-base text-center font-medium font-ralewayFont">
+                    {producto.nomProducto}
+                  </td>
+                  <td className="px-4 py-3 text-base text-center font-medium font-ralewayFont">
+                    {producto.cantidadTotal}
+                  </td>
+                  <td className="px-4 py-3 text-base text-center font-medium font-ralewayFont">
+                    <button
+                      className="font-medium text-blue-600 dark:text-blue-500 hover:underline px-2 py-1 text-base"
+                      onClick={() => handleNavigateEditar(producto.idProducto)}
+                    >
+                      Editar
+                    </button>
+                    <button
+                      className="font-medium text-red-600 dark:text-blue-500 hover:underline px-2 py-2 text-base font-ralewayFont"
+                      onClick={() =>
+                        handleNavigateEliminar(producto.idProducto)
+                      }
+                    >
+                      Eliminar
+                    </button>
+                    <button
+                      className="font-medium text-green-600 dark:text-blue-500 hover:underline px-2 py-2 text-base font-ralewayFont"
+                      onClick={() => handleNavigateDetalle(producto.idProducto)}
+                    >
+                      Detalle
+                    </button>
+                  </td>     
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </main>
     </>
   );
 }
